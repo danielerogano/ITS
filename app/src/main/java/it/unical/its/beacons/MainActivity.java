6fff63c29ee8f9b5e2370ea2337547c94871f93b
Daniele <danielerogano@gmail.com>
2016-03-10 22:10:49 +0100

    Rejected commit: Default

--- app/src/main/java/it/unical/its/beacons/MainActivity.java
+++ app/src/main/java/it/unical/its/beacons/MainActivity.java
@@ -2,6 +2,7 @@ package it.unical.its.beacons;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
@@ -9,7 +10,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.altbeacon.beacon.*;
@@ -21,7 +24,9 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
     public static final int REQUEST_BLUETOOTH_ENABLE = 1;
     private BeaconManager beaconManager;
     public static final String TAG = "Presence";
-    public TextView text;
+    public TextView text, txt1, txt2, txt3;
+    public ImageView img1, img2, img3, img4;
+    public double d1, d2, d3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,34 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         setContentView(R.layout.activity_main);
 
         text = (TextView)findViewById(R.id.textView);
+        txt1 = (TextView)findViewById(R.id.textViewB1value);
+        txt2 = (TextView)findViewById(R.id.textViewB2value);
+        txt3 = (TextView)findViewById(R.id.textViewB3value);
+
+        img1 = (ImageView) findViewById(R.id.imageView);
+        img1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayMessage("Area 1");
+            }
+        });
+        img2 = (ImageView) findViewById(R.id.imageView2);
+        img2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayMessage("Area 2");
+            }
+        });
+        img3 = (ImageView) findViewById(R.id.imageView3);
+        img3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayMessage("Area 3");
+            }
+        });
+        img4 = (ImageView) findViewById(R.id.imageView4);
+        img4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayMessage("Area 4");
+            }
+        });
 
         verifyBluetooth();
 
@@ -93,7 +126,7 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
                 try {
                     Log.d(TAG, "Enter Region");
                     // Toast.makeText(getApplicationContext(), "Enter Region", Toast.LENGTH_SHORT).show();
-                    displayMessage("Enter Region");
+                    displayMessage("Benvenuto in ufficio!");
                     beaconManager.startRangingBeaconsInRegion(region);
                 } catch (RemoteException e) {
                     e.printStackTrace();
@@ -106,7 +139,7 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
                 try {
                     Log.d(TAG, "Exit Region");
                     // Toast.makeText(getApplicationContext(), "Exit Region", Toast.LENGTH_SHORT).show();
-                    displayMessage("Exit Region");
+                    displayMessage("Arrivederci!");
                     beaconManager.stopRangingBeaconsInRegion(region);
                 } catch (RemoteException e) {
                     e.printStackTrace();
@@ -123,31 +156,47 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                //   Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                   //Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
                 for (Beacon oneBeacon : beacons) {
-                    if (oneBeacon.getId2().equals(Identifier.parse("2")) && oneBeacon.getId3().equals(Identifier.parse("1"))) {
+
+                    if (oneBeacon.getId2().equals(Identifier.parse("1")) && oneBeacon.getId3().equals(Identifier.parse("1"))) {
+                        d1=oneBeacon.getDistance();
+                        updateText1("" + d1);
+                    }
+                    if (oneBeacon.getId2().equals(Identifier.parse("1")) && oneBeacon.getId3().equals(Identifier.parse("2"))) {
+                        d2=oneBeacon.getDistance();
+                        updateText2("" + d2);
+                    }
+                    if (oneBeacon.getId2().equals(Identifier.parse("1")) && oneBeacon.getId3().equals(Identifier.parse("3"))) {
+                        d3=oneBeacon.getDistance();
+                        updateText3("" + d3);
+                    }
+
+/*
+                    if (oneBeacon.getId2().equals(Identifier.parse("2")) && oneBeacon.getId3().equals(Identifier.parse("3"))) {
                         Log.d(TAG, "Beacon: " + oneBeacon.getId2() + " - " + oneBeacon.getId3() + " - Distance: " + oneBeacon.getDistance() + " - Rssi: " + oneBeacon.getRssi() + " - TxPower: " + oneBeacon.getTxPower());
                     }
                     // Log.d(TAG, "Distance: " + oneBeacon.getDistance() + "!");
 
                     if (oneBeacon.getDistance() < 1.0 && oneBeacon.getId2().equals(Identifier.parse("1")) && oneBeacon.getId3().equals(Identifier.parse("1"))) {
-                        updateText("Beacon PRO 1");
+                        updateText("Beacon PRO 1 - Distance: " + oneBeacon.getDistance());
                     }
                     if (oneBeacon.getDistance() < 1.0 && oneBeacon.getId2().equals(Identifier.parse("1")) && oneBeacon.getId3().equals(Identifier.parse("2"))) {
-                        updateText("Beacon PRO 2");
+                        updateText("Beacon PRO 2 - Distance: " + oneBeacon.getDistance());
                     }
                     if (oneBeacon.getDistance() < 1.0 && oneBeacon.getId2().equals(Identifier.parse("1")) && oneBeacon.getId3().equals(Identifier.parse("3"))) {
-                        updateText("Beacon PRO 3");
+                        updateText("Beacon PRO 3 - Distance: " + oneBeacon.getDistance());
                     }
                     if (oneBeacon.getDistance() < 1.0 && oneBeacon.getId2().equals(Identifier.parse("2")) && oneBeacon.getId3().equals(Identifier.parse("1"))) {
-                        updateText("Beacon mini 1");
+                        updateText("Beacon mini 1 - Distance: " + oneBeacon.getDistance());
                     }
                     if (oneBeacon.getDistance() < 1.0 && oneBeacon.getId2().equals(Identifier.parse("2")) && oneBeacon.getId3().equals(Identifier.parse("2"))) {
-                        updateText("Beacon mini 2");
+                        updateText("Beacon mini 2 - Distance: " + oneBeacon.getDistance());
                     }
                     if (oneBeacon.getDistance() < 1.0 && oneBeacon.getId2().equals(Identifier.parse("2")) && oneBeacon.getId3().equals(Identifier.parse("3"))) {
-                        updateText("Beacon mini 3");
+                        updateText("Beacon mini 3 - Distance: " + oneBeacon.getDistance());
                     }
+*/
                 }
             }
         });
@@ -166,6 +215,36 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
                 text = (TextView) MainActivity.this
                         .findViewById(R.id.textView);
                 text.setText(line);
+            }
+        });
+    }
+
+    public void updateText1(final String line) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                txt1 = (TextView) MainActivity.this
+                        .findViewById(R.id.textViewB1value);
+                txt1.setText(line);
+            }
+        });
+    }
+
+    public void updateText2(final String line) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                txt2 = (TextView) MainActivity.this
+                        .findViewById(R.id.textViewB2value);
+                txt2.setText(line);
+            }
+        });
+    }
+
+    public void updateText3(final String line) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                txt3 = (TextView) MainActivity.this
+                        .findViewById(R.id.textViewB3value);
+                txt3.setText(line);
             }
         });
     }
